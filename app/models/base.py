288083632084ff1eff6299bc11c999d83b5ddd35
RocketMenace <grizzlygit777@gmail.com>
2025-08-19 19:26:43 +0300

    build: Implemented models.

---
+++ app/models/base.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import mapped_column, Mapped
+from uuid import UUID, uuid4
+from sqlalchemy import DateTime
+from app.config.database import database
+from datetime import datetime
+from sqlalchemy.sql import func
+
+
+class BaseModel(database.Base):
+    __abstract__ = True
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4, index=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+        onupdate=func.now(),
+    )
---
+++ app/models/task.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, Enum as SQLEnum
+from enum import Enum
+
+from .base import BaseModel
+
+
+class StatusType(Enum):
+    CREATED = "Created"
+    IN_PROGRESS = "In progress"
+    COMPLETED = "Completed"
+
+
+class Task(BaseModel):
+    __tablename__ = "tasks"
+
+    name: Mapped[str] = mapped_column(String(length=255))
+    description: Mapped[str] = mapped_column(Text)
+    status: Mapped[StatusType] = mapped_column(
+        SQLEnum(StatusType),
+    )
